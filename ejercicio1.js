1cfa73f5a0944d67c78fb42323094a9f907b19d4
stiveNNgomeZ <nestorstivengomez@gmail.com>
2026-03-27 17:32:33 -0500

    cree el ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,12 @@
+// Crear lista con 5 nombres
+let aprendices = ["Ana", "Luis", "Carlos", "Marta", "Sofia"];
+
+// Agregar un nuevo nombre al final
+aprendices.push("Pedro");
+
+// Eliminar el primer nombre
+aprendices.shift();
+
+// Mostrar cantidad total
+console.log(aprendices.length);
+
